e4a3443de291a4fed0899313b67df59ae6718324
neyaz <neyazmorshid@gmail.com>
2025-03-11 02:42:45 +0600

    inital basic setup is done , it is connected with the telegram bot , it accepts the msg

--- bot.js
+++ bot.js
@@ -1,21 +1,29 @@
 require("dotenv").config();
+const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
+const PORT = process.env.PORT || 9000
+const app = express();
+// https://api.telegram.org/bot${MY_TOKEN}/${method}
+//  https://api.telegram.org/bot7929468904:AAFZuONyBz-92uhSnatUN_b6XTll50c6ydk/setWebhook?url=https://a67e-103-87-214-195.ngrok-free.app
 
-// .env ফাইল থেকে BOT_TOKEN লোড করা
-const token = process.env.BOT_TOKEN;
+// টোকেন লোড করা
+const token = process.env.TELEGRAM_BOT_TOKEN;
 
-// বট ইনিশিয়ালাইজ করা (Polling method)
-const bot = new TelegramBot(token, { polling: true });
+// Polling mode চালু করা (Webhook ছাড়া বট কাজ করানোর জন্য)
+// const bot = new TelegramBot(token, { polling: true });
+app.use(express.json());
+app.post('*',async (req,res) => {
+    console.log(req.body)
+    res.send('Post ...')
+})
 
-// যখন কেউ /start কমান্ড পাঠাবে
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, `Hello ${msg.chat.first_name}! 👋 আমি তোমার Telegram Bot`);
-});
+app.get('*',async (req,res) => {
+    // console.log(req.body)
+    res.send(' Get ...')
+})
 
-// যখন কেউ মেসেজ পাঠাবে
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  if (msg.text.toLowerCase() === "hello") {
-    bot.sendMessage(chatId, "Hi there! 😊");
-  }
-});
+app.listen(PORT, function (err) {
+    if(err) console.log(err);
+    console.log("Server listening on port, " , PORT)
+    console.log("🤖 বট চলছে...")
+} )
